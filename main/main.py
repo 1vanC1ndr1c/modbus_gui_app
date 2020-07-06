5bb59e028c17341e2a8f9debbdbd0a482b992d9e
ivan.cindric <ivan.cindric95@gmail.com>
2020-07-06 16:50:12 +0200

    "Test Commit"

--- main/main.py
+++ main/main.py
@@ -1,1 +1,37 @@
-print ("Hello")
+import os
+import asyncio
+import aiohttp.web
+import contextlib
+import json
+import sys
+import subprocess
+import socket
+from multiprocessing import Process
+import websocket
+
+
+
+async def connect_to_socket():
+    session = aiohttp.ClientSession()
+    ws = await session.ws_connect('ws://localhost:3456/ws')
+
+    while True:
+        msg = await ws.receive()
+        if msg.tp == aiohttp.MsgType.text:
+            if msg.data == 'close':
+                await ws.close()
+                break
+            else:
+                await ws.send_str(msg.data + '/answer')
+        elif msg.tp == aiohttp.MsgType.closed:
+            break
+        elif msg.tp == aiohttp.MsgType.error:
+            break
+
+
+# async def write_loop():
+# while True:
+# ws.send_bytes(b'000100000006090300040001')
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(connect_to_socket())
---
+++ main/wstunnel.py
@@ -0,0 +1,69 @@
+import asyncio
+import aiohttp.web
+import contextlib
+import json
+import sys
+
+
+conf = {
+    'ws': {
+        'host': '0.0.0.0',
+        'port': 3456},
+    'modbus': {
+        'host': 'localhost',
+        'port': 502}}
+
+
+async def ws_handler(request):
+    ws = aiohttp.web.WebSocketResponse()
+    await ws.prepare(request)
+    try:
+        conn_msg_str = await ws.receive_str()
+        conn_msg = json.loads(conn_msg_str)
+        reader, writer = await asyncio.open_connection(
+            conf['modubs']['host'], conf['modbus']['port'])
+    except Exception:
+        return ws
+    await ws.send_str('ACK')
+
+    async def tcp_read_loop():
+        while True:
+            try:
+                msg = await reader.read(1024)
+            except Exception:
+                break
+            if not msg:
+                break
+            await ws.send_bytes(msg)
+
+    async def ws_read_loop():
+        while True:
+            try:
+                msg = await ws.receive_bytes()
+            except Exception:
+                break
+            writer.write(msg)
+
+    tcp_future = asyncio.ensure_future(tcp_read_loop())
+    ws_future = asyncio.ensure_future(ws_read_loop())
+    await asyncio.wait(
+        [tcp_future, ws_future], return_when=asyncio.FIRST_COMPLETED)
+    tcp_future.cancel()
+    ws_future.cancel()
+
+    with contextlib.suppress(Exception):
+        writer.write_eof()
+    with contextlib.suppress(Exception):
+        writer.close()
+    return ws
+
+
+def main():
+    app = aiohttp.web.Application()
+    app.router.add_route('*', '/ws', ws_handler)
+    aiohttp.web.run_app(app,
+                        host=conf['ws']['host'], port=conf['ws']['port'],
+                        shutdown_timeout=0)
+
+if __name__ == '__main__':
+    sys.exit(main())
